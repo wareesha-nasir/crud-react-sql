dd1f3d1b9bd1f2e7087d58b3a910b64e135caccf
wareesha-nasir <warisha.nasir356@gmail.com>
2021-10-17 00:37:47 +0500

    state variables

--- server/index.js
+++ server/index.js
@@ -1,8 +1,35 @@
 const express= require('express')
-const app=express()
+const app=express();
+const bodyParser=require("body-parser")
+const mysql=require("mysql")
+//install cors
+//const cors=require("cors")
 
-app.listen(3000)
-
-app.get('/',(req,res)=>{
-res.send('App')
+const db=mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"hope356",
+    database:"movies_details"
 })
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.json())
+
+app.get('/api/reviews',(req,res)=>{
+    const selectQuery="SELECT * FROM movie_info";
+    db.query(selectQuery,(err,result)=>{
+      res.send(result);
+    })
+})
+
+app.post('/api/postreview',(req,res)=>{
+    const movieName=req.body.movieName;
+    const movieReview=req.body.movieReview;
+   const InstSql="INSERT INTO movie_info (movie_name,movie_review) VALUES (?,?)"
+    db.query(InstSql,[movieName,movieReview],(err,result)=>{
+//res.send("data inserted")
+console.log(result)
+    })
+
+})
+app.listen(3000)
